75e87537e73de29ad2a308925d3a898e9d7ed84c
Moojong Song <purechris@me.com>
2018-04-13 15:54:55 +0900

    feat: add model classed
    add model classed
    + ImageCell : It is use to collection cell
    + ImageCellItem: it is use to collection cell data item
    + ImageObject: it is use to image data managing

---
+++ GettyImageViewer/Classes/HomeView/HomeImageCell.swift
@@ -0,0 +1,13 @@
+//
+//  HomeImageCell.swift
+//  GettyImageViewer
+//
+//  Created by Chris Song on 13/04/2018.
+//  Copyright © 2018 ChrisMJSong. All rights reserved.
+//
+
+import UIKit
+
+class HomeImageCell: UICollectionViewCell {
+
+}
---
+++ GettyImageViewer/Classes/HomeView/HomeImageCellItem.swift
@@ -0,0 +1,13 @@
+//
+//  HomeImageCellItem.swift
+//  GettyImageViewer
+//
+//  Created by Chris Song on 13/04/2018.
+//  Copyright © 2018 ChrisMJSong. All rights reserved.
+//
+
+import UIKit
+
+class HomeImageCellItem: NSObject {
+
+}
