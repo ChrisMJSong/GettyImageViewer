9d92e222e798ec82b82d404dc2cc879cbc5b6c3a
Moojong Song <purechris@me.com>
2018-04-13 16:13:20 +0900

    feat: Add detailViewController
    add detailViewController

---
+++ GettyImageViewer/Classes/DetailView/DetailViewController.swift
@@ -0,0 +1,24 @@
+//
+//  DetailViewController.swift
+//  GettyImageViewer
+//
+//  Created by Chris Song on 13/04/2018.
+//  Copyright © 2018 ChrisMJSong. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
