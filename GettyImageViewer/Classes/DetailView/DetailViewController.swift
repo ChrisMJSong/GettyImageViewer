f6e603705e689b01b0f52cffc0ab7c4e2c0a6967
Moojong Song <purechris@me.com>
2018-04-13 19:49:37 +0900

    feat: add tap gesture for full screen
    add tap gesture function for full screen
    if tap the screen, will show or hide navigation view.

--- GettyImageViewer/Classes/DetailView/DetailViewController.swift
+++ GettyImageViewer/Classes/DetailView/DetailViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+    
+    fileprivate let animateTime = 0.25
 
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var imageView: UIImageView!
@@ -23,13 +25,43 @@ class DetailViewController: UIViewController {
         self.title = item?.subject
         
         self.imageView.image = item?.imageObject.loadImage()
+        
+        // set tap gesture for hide navigation
+        let tapGesture = UITapGestureRecognizer.init(target: self, action: #selector(toggleNavigationBar(_ :)))
+        self.scrollView.addGestureRecognizer(tapGesture)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    /// Hide navigation bar and toolbar when tapped
+    ///
+    /// - Parameter gesture: TapGesture instance
+    @objc func toggleNavigationBar(_ gesture: UITapGestureRecognizer) {
+        guard let isHidden = self.navigationController?.isNavigationBarHidden else {
+            return
+        }
+        self.navigationController?.setNavigationBarHidden(!isHidden, animated: true)
+        
+        // Hide toolbar animation
+        let animateTime = isHidden ? self.animateTime : 0
+        UIView.animate(withDuration: animateTime) {
+            self.toolBar.alpha = CGFloat(isHidden ? 1 : 0)
+        }
+        
+        // change background color
+        self.scrollView.backgroundColor = isHidden ? UIColor.white : UIColor.black
+    }
 
+    /// Hide statusbar when full screen imaging
+    override var prefersStatusBarHidden: Bool {
+        guard let currentState = self.navigationController?.isNavigationBarHidden else {
+            return false
+        }
+        return currentState
+    }
 }
 
 // MARK: UIScrollViewDelegate
