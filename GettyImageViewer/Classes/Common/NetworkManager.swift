5361ddbe406bd1b6b64530dde295e34aa7a2e6e1
Moojong Song <purechris@me.com>
2018-04-15 05:24:31 +0900

    feat: implement image object
    implement image object class and network.

--- GettyImageViewer/Classes/Common/NetworkManager.swift
+++ GettyImageViewer/Classes/Common/NetworkManager.swift
@@ -8,18 +8,88 @@
 
 import UIKit
 import RxSwift
-import Alamofire
-import SwiftyJSON
+import RxAlamofire
+import HTMLReader
+import SVProgressHUD
+
+public typealias CompletionHandler = (_ result: String?, _ error: NetworkError?) -> Void
+public typealias CompletionDataHandler = (_ result: Data?, _ error: NetworkError?) -> Void
+
+public enum NetworkError: Int {
+    case connection = -1009, ats = -1022, hostNotFound = -1003, unknown = 9999
+    
+    func localzedDescription() -> String {
+        switch self {
+        case .connection:  return NSLocalizedString("The Internet connection appears to be offline.", comment: "NetworkError - connection")
+        case .ats:  return NSLocalizedString("The resource could not be loaded because the App Transport Security policy requires the use of a secure connection.", comment: "NetworkError - ats")
+        case .hostNotFound:  return NSLocalizedString("A server with the specified hostname could not be found.", comment: "NetworkError - ats")
+        case .unknown:  return NSLocalizedString("Unknown network error.", comment: "NetworkError - unknown")
+        }
+    }
+}
 
 class NetworkManager: NSObject {
-    static let shared = NetworkManager()
-    var sessionManager: Alamofire.SessionManager? = nil
     
-    private override init(){
-        let configuration = URLSessionConfiguration.default
-        configuration.timeoutIntervalForRequest  = 60
-        configuration.timeoutIntervalForResource = 60
+    static let serverUrl = "http://www.gettyimagesgallery.com/"
+    static let shared = NetworkManager()
+    let disposeBag = DisposeBag()
+    
+    /// get page source from web page
+    ///
+    /// - Parameters:
+    ///   - requestUrlString: target webpage
+    ///   - completion: completion closure
+    func stringFromWebPageSource(_ requestUrlString: String, _ completion: @escaping CompletionHandler){
         
-        sessionManager = Alamofire.SessionManager(configuration: configuration)
+        // show load indicator
+        SVProgressHUD.show()
+        RxAlamofire.requestString(.get, requestUrlString)
+            .subscribe(onNext: { (respons, resultString) in
+                completion(resultString, nil)
+            }, onError: { (error) in
+                SVProgressHUD.dismiss()
+                let objError = error as NSError
+                let nError = NetworkError.init(rawValue: objError.code)
+                completion(nil, nError)
+            }, onCompleted: {
+                // hide load indicoatr
+                SVProgressHUD.dismiss()
+            }, onDisposed: nil)
+            .disposed(by: disposeBag)
+    }
+    
+    /// get getty page source string
+    ///
+    /// - Parameter completion: completion closure
+    func loadGettyMainPageSource(_ completion: @escaping CompletionHandler){
+        let requestUrlString = NetworkManager.serverUrl + "collections/archive/slim-aarons.aspx"
+        stringFromWebPageSource(requestUrlString, completion)
+    }
+    
+    /// get getty page source at detail page
+    ///
+    /// - Parameters:
+    ///   - subUrlString: detail page url
+    ///   - completion: completion closure
+    func loadGettyDetailpageSource(subUrl subUrlString: String, _ completion: @escaping CompletionHandler){
+        let requestUrlString = NetworkManager.serverUrl + subUrlString
+        stringFromWebPageSource(requestUrlString, completion)
+    }
+    
+    /// download image from url
+    ///
+    /// - Parameters:
+    ///   - urlString: image url
+    ///   - completion: completion closure
+    func downloadImage(_ urlString: String, _ completion: @escaping CompletionDataHandler) {
+        let requestUrlString = NetworkManager.serverUrl + urlString
+        
+        RxAlamofire.requestData(.get, requestUrlString).subscribe(onNext: { (response, data) in
+            completion(data, nil)
+        }, onError: { (error) in
+            let objError = error as NSError
+            let nError = NetworkError.init(rawValue: objError.code)
+            completion(nil, nError)
+        }, onCompleted: nil, onDisposed: nil).disposed(by: disposeBag)
     }
 }
--- GettyImageViewer/Classes/HomeView/HomeImageObject.swift
+++ GettyImageViewer/Classes/HomeView/HomeImageObject.swift
@@ -9,23 +9,244 @@
 import UIKit
 
 class HomeImageObject: NSObject {
+    
+    /// image content type
+    ///
+    /// - thumbnail: thumbnail image
+    /// - origin: original image
+    enum ImageContentType: Int {
+        case thumbnail = 0, origin
+        
+        /// return enum string value
+        ///
+        /// - Returns: a string that enum value
+        func string() -> String {
+            switch self {
+            case .thumbnail:    return "cachedImageThumb"
+            case .origin:       return "cachedImageOrigin"
+            }
+        }
+    }
 
-    var urlString: String?
+    var thumbImageUrlString: String?
+    var originImageUrlString: String?
     var cache: NSCache = NSCache<NSString, UIImage>()
     
-    func loadThumbnailImage() -> UIImage {
-        // 1. check cache
-        // 2. check local
-        // 3. web download
+    static let thumbnailDirectoryName = "thumbnails"
+    static let originalDirectoryName  = "originals"
+    
+    /// Return a image
+    ///
+    /// - Parameters:
+    ///   - type: content type
+    ///   - imageView: set image to this imageview after download, if you need.
+    /// - Returns: image instance
+    func loadImage(_ type: ImageContentType, imageView: UIImageView?) -> UIImage? {
         
-        return #imageLiteral(resourceName: "ImageSample.jpg")
+        // 1. want origin type
+        // 1-1. check cache
+        // 1-2. check local
+        // 1-2-1. temporary return thumbnail (go 2)
+        // 1-3. webdownload
+        // 1-4. show image after download
+        // 2. want thumbnail
+        // 2-1. check cache
+        // 2-2. check local
+        // 2-3. webdownload
+        // 2-4. show image after download
+        
+        var image = self.imageFromCache(type)
+        guard image == nil else {
+            // found from cache
+            return image
+        }
+        image = self.imageFromLocal(type)
+        guard image == nil else {
+            // found from local
+            return image
+        }
+        
+        if let cell = imageView?.superview?.superview, cell is HomeImageCell {
+            // get indexpath
+        }
+        
+        // before get image from web
+        if type == .origin {
+            // load thumb image
+            imageView?.image = self.loadImage(.thumbnail, imageView: imageView)
+        }
+        // try get image from web
+        var targetUrlString: String?
+        switch type {
+        case .thumbnail:
+            targetUrlString = self.thumbImageUrlString
+        case .origin:
+            targetUrlString = self.originImageUrlString
+        }
+        
+        guard let target = targetUrlString else {
+            // cant find target url
+            return nil
+        }
+        
+        NetworkManager.shared.downloadImage(target) { (data, error) in
+            guard let data = data else {
+                return
+            }
+            let image = UIImage.init(data: data)
+            let key = NSString.init(string: type.string())
+            self.cache.setObject(image!, forKey: key)
+            
+            imageView?.image = image
+            
+            // save image
+            let result = self.setImageToLocal(type: type, imageData: data)
+            if result == false {
+                // need more space
+            }
+        }
+        
+        return image
     }
     
-    func loadImage() -> UIImage {
-        // 1. check cache
-        // 2. check local
-        // 3. web download
+    /// get image from cache
+    ///
+    /// - Parameter type: image type
+    /// - Returns: image instance
+    func imageFromCache(_ type: ImageContentType) -> UIImage? {
+        let key = NSString.init(string: type.string())
+        return self.cache.object(forKey: key)
+    }
+    
+    /// set image to cache
+    ///
+    /// - Parameters:
+    ///   - image: image instance
+    ///   - type: image type
+    func setImageToCache(image: UIImage, imageType type: ImageContentType) {
+        let key = NSString.init(string: type.string())
+        self.cache.setObject(image, forKey: key)
+    }
+    
+    /// return a url that cache directory path
+    ///
+    /// - Returns: cache directory path
+    func cacheDirectoryPath() -> URL? {
+        let paths = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask)
+        let cacheDirPath = paths.last
+        return cacheDirPath
+    }
+    
+    /// return a url that image sub path by content type
+    ///
+    /// - Parameter type: content type
+    /// - Returns: sub url path
+    func imagePath(_ type: ImageContentType) -> URL? {
+        guard let cacheDirectory = cacheDirectoryPath() else {
+            return nil
+        }
         
-        return #imageLiteral(resourceName: "ImageSample.jpg")
+        var targetDriName = ""
+        switch type {
+        case .thumbnail:
+            targetDriName = HomeImageObject.thumbnailDirectoryName
+        case .origin:
+            targetDriName = HomeImageObject.originalDirectoryName
+        }
+        
+        let imagePath = cacheDirectory.appendingPathComponent("images").appendingPathComponent(targetDriName)
+        
+        if !FileManager.default.fileExists(atPath: imagePath.absoluteString) {
+            // if cant find create image directory
+            do {
+                try FileManager.default.createDirectory(at: imagePath, withIntermediateDirectories: true, attributes: nil)
+            }
+            catch {
+                // create fail
+                return nil
+            }
+        }
+        
+        return imagePath
+    }
+    
+    /// return a file name by content type
+    ///
+    /// - Parameter type: content type
+    /// - Returns: file name with extension
+    func fileName(_ type: ImageContentType) -> String? {
+        var urlSubPath: String?
+        switch type {
+        case .thumbnail:
+            urlSubPath = self.thumbImageUrlString
+        case .origin:
+            urlSubPath = self.originImageUrlString
+        }
+        
+        guard let urlPath = urlSubPath else {
+            return nil
+        }
+        
+        guard let subPath = URL.init(string: urlPath) else {
+            return nil
+        }
+        let fileName = subPath.lastPathComponent
+        
+        return fileName
+    }
+    
+    /// return stored image full path string
+    ///
+    /// - Parameter type: content type
+    /// - Returns: image path string
+    func imageFilePath(_ type: ImageContentType) -> String? {
+        let imagePath = self.imagePath(type)
+        let fileName  = self.fileName(type)
+        
+        guard let fileName2 = fileName else {
+            return nil
+        }
+        
+        let fullPath = imagePath?.appendingPathComponent(fileName2)
+        
+        return fullPath?.absoluteString
+    }
+    
+    /// return a stored image from local
+    ///
+    /// - Parameter type: image content type
+    /// - Returns: image instance
+    func imageFromLocal(_ type: ImageContentType) -> UIImage? {
+        guard let path = imageFilePath(type) else {
+            return nil
+        }
+        
+        guard let data = FileManager.default.contents(atPath: path) else {
+            return nil
+        }
+        let image = UIImage.init(data: data)
+        
+        return image
+    }
+    
+    /// save image data to local
+    ///
+    /// - Parameters:
+    ///   - type: image content type
+    ///   - imageData: image data
+    /// - Returns: save success state
+    func setImageToLocal(type: ImageContentType, imageData: Data) -> Bool {
+        guard let path = imageFilePath(type) else {
+            return false
+        }
+        
+        do {
+         try imageData.write(to: path.asURL())
+        }catch{
+            // write fail
+            return false
+        }
+        
+        return true
     }
 }
